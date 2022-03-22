68685f628f88037520121ee5d6009afaf09b0691
Ken Woodward <ken@hoodoo.digital>
2022-03-22 14:07:53 -0600

    removed boilerplate header css

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -54,16 +54,16 @@ export default async function decorate(block) {
   nav.append(navSections);
 
   // hamburger for mobile
-  const hamburger = document.createElement('div');
-  hamburger.classList.add('nav-hamburger');
-  hamburger.innerHTML = '<div class="nav-hamburger-icon"></div>';
-  hamburger.addEventListener('click', () => {
-    const expanded = nav.getAttribute('aria-expanded') === 'true';
-    document.body.style.overflowY = expanded ? '' : 'hidden';
-    nav.setAttribute('aria-expanded', expanded ? 'false' : 'true');
-  });
-  nav.prepend(hamburger);
-  nav.setAttribute('aria-expanded', 'false');
+  // const hamburger = document.createElement('div');
+  // hamburger.classList.add('nav-hamburger');
+  // hamburger.innerHTML = '<div class="nav-hamburger-icon"></div>';
+  // hamburger.addEventListener('click', () => {
+  //   const expanded = nav.getAttribute('aria-expanded') === 'true';
+  //   document.body.style.overflowY = expanded ? '' : 'hidden';
+  //   nav.setAttribute('aria-expanded', expanded ? 'false' : 'true');
+  // });
+  // nav.prepend(hamburger);
+  // nav.setAttribute('aria-expanded', 'false');
 
   block.append(nav);
 }
